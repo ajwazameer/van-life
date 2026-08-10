e5d2ca35650994f58328e13f2eb0904b4294ee45
ajwazameer <ajwazameer2004@gmail.com>
2026-08-10 18:44:02 +0500

    fix: margin above back to vans button

--- src/pages/vans/VanDetail.jsx
+++ src/pages/vans/VanDetail.jsx
@@ -1,4 +1,4 @@
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 import { useEffect, useState } from "react";
 export default function VanDetail() {
   const [van, setVan] = useState(null);
@@ -11,16 +11,21 @@ export default function VanDetail() {
   return (
     <div className="van-detail-container">
       {van ? (
-        <div className="van-detail">
-          <img src={van.imageUrl} />
-          <i className={`van-type ${van.type} selected`}>{van.type}</i>
-          <h2>{van.name}</h2>
-          <p className="van-price">
-            <span>${van.price}</span>/day
-          </p>
-          <p>{van.description}</p>
-          <button className="link-button">Rent this van</button>
-        </div>
+        <>
+          <Link to=".." relative="path" className="back-button">
+            &larr; <span>Back to all vans</span>
+          </Link>
+          <div className="van-detail">
+            <img src={van.imageUrl} />
+            <i className={`van-type ${van.type} selected`}>{van.type}</i>
+            <h2>{van.name}</h2>
+            <p className="van-price">
+              <span>${van.price}</span>/day
+            </p>
+            <p>{van.description}</p>
+            <button className="link-button">Rent this van</button>
+          </div>
+        </>
       ) : (
         <h2>Loading...</h2>
       )}
