dfea1918bcade843c25ac9edabf0d2cead1e9928
ajwazameer <ajwazameer2004@gmail.com>
2026-08-11 18:19:49 +0500

    feat: filter buttons implementation

--- src/pages/vans/Vans.jsx
+++ src/pages/vans/Vans.jsx
@@ -1,15 +1,20 @@
 import clsx from "clsx";
 import { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
+import { Link, useSearchParams, NavLink } from "react-router-dom";
 export default function Vans() {
   const [vansList, setVanList] = useState([]);
+  const [searchParams, setSearchParams] = useSearchParams();
+  const typeFilter = searchParams.get("type");
+  console.log(typeFilter);
   useEffect(() => {
     fetch("/api/vans")
       .then((res) => res.json())
       .then((data) => setVanList(data.vans));
   }, []);
-  //   localStorage.setItem("vansList", vansList);
-  const vans = vansList.map((van) => {
+  const filteredList = typeFilter
+    ? vansList.filter((van) => van.type.toLowerCase() === typeFilter)
+    : vansList;
+  const vans = filteredList.map((van) => {
     return (
       <div key={van.id} className="vans-tile">
         <Link to={`/vans/${van.id}`}>
@@ -26,6 +31,7 @@ export default function Vans() {
           </div>
           <span
             className={clsx("vans-type", {
+              simple: "simple" === van.type,
               luxury: "luxury" === van.type,
               rugged: "rugged" === van.type,
             })}
@@ -43,11 +49,19 @@ export default function Vans() {
         <h1>Explore our van options</h1>
         <div className="filter-section">
           <div className="filter-buttons">
-            <span>Simple</span>
-            <span>Luxury</span>
-            <span>Rugged</span>
+            <NavLink to="?type=simple" className="vans-type sim">
+              <span>Simple</span>
+            </NavLink>
+            <NavLink to="?type=rugged" className="vans-type rugg">
+              <span>Rugged</span>
+            </NavLink>
+            <NavLink to="?type=luxury" className="vans-type lux">
+              <span>Luxury</span>
+            </NavLink>
           </div>
-          <button>Clear Filters</button>
+          <Link to=".">
+            <span>Clear Filters</span>
+          </Link>
         </div>
       </header>
 
